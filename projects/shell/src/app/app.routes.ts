60f922d8f70785758560e1883e8e8c9ac1a1d691
Aaron Gonzales <aaronzinho@ucla.edu>
2023-08-08 08:33:23 -0700

    feat: init commit

---
+++ projects/shell/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+
+  // Your route here:
+
+  {
+    path: 'flights',
+    loadChildren: () => import('mfe1/Module').then((m) => m.FlightsModule)
+  },
+
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
